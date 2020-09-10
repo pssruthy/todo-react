896af91449d35c522fdebed9e019496b35082a14
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-09-10 12:16:51 +0530

    Made data persistent
    - Used redis to make the data persistent

--- server/app.js
+++ server/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const redis = require('redis');
+
+const Db = require('./Db');
 const { getDefaultStatus, getNextStatus } = require('./status');
 
+const REDIS_URL = 6379;
 const app = express();
-
-app.locals.todo = {
-  heading: 'Todo',
-  items: [],
-  lastId: 0,
-};
+const redisClient = redis.createClient(REDIS_URL);
+const db = new Db(redisClient);
 
 app.use(express.json());
+
+db.getTodo().then((todo) => {
+  const defaultTodo = { heading: 'Todo', items: [], lastId: 0 };
+  app.locals.todo = todo || defaultTodo;
+});
 
 app.get('/api/getHeading', (req, res) => {
   res.send(JSON.stringify({ heading: app.locals.todo.heading }));
@@ -21,6 +26,7 @@ app.get('/api/getAllItems', (req, res) => {
 
 app.post('/api/updateHeading', (req, res) => {
   app.locals.todo.heading = req.body.heading;
+  db.setTodo(app.locals.todo);
   res.end();
 });
 
@@ -29,6 +35,7 @@ app.post('/api/addItem', (req, res) => {
   const { items, lastId } = app.locals.todo;
   items.push({ item, id: lastId + 1, status: getDefaultStatus() });
   app.locals.todo.lastId++;
+  db.setTodo(app.locals.todo);
   res.end();
 });
 
@@ -36,11 +43,13 @@ app.post('/api/removeItem', (req, res) => {
   const { itemId } = req.body;
   const { todo } = app.locals;
   todo.items = todo.items.filter((item) => item.id !== itemId);
+  db.setTodo(app.locals.todo);
   res.end();
 });
 
 app.post('/api/removeTodo', (req, res) => {
   app.locals.todo = { heading: 'Todo', items: [], lastId: 0 };
+  db.setTodo(app.locals.todo);
   res.end();
 });
 
@@ -49,6 +58,7 @@ app.post('/api/toggleItemStatus', (req, res) => {
   const { todo } = app.locals;
   const itemToToggle = todo.items.find(({ id }) => id === itemId);
   itemToToggle.status = getNextStatus(itemToToggle.status);
+  db.setTodo(app.locals.todo);
   res.end();
 });
 
